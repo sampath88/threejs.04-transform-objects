4c8a94c65535663a9b4815970d1b28e75633cd6e
sampath88 <sampathreddy8896@gmail.com>
2024-07-14 13:58:14 +0530

    Grouping Objects

--- src/script.js
+++ src/script.js
@@ -9,28 +9,32 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  //   wireframe: true,
-});
-const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1);
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 0.5;
+group.rotation.y = Math.PI * 0.25;
+scene.add(group);
 
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
 
-// Rotation
-mesh.rotation.reorder("YXZ");
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
-scene.add(mesh);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2);
@@ -49,11 +53,7 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-// camera.position.y = 1;
-// camera.position.x = 1;
 scene.add(camera);
-
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
